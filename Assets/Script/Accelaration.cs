ff1caf22d847b2d5a053fa07b77e5ba79d69e0bc
Sonwonill <tlatmsdkqk@gmail.com>
2024-10-05 21:36:57 +0900

    :sparkles: Feat : "중력 가속도 개선 및 합력 기능 추가"
    가속도 적용을 오일러 방법이 아닌 수정된 오일러 방법으로 개선

--- Assets/Script/Accelaration.cs
+++ Assets/Script/Accelaration.cs
@@ -1,40 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Accelaration : MonoBehaviour
 {
-    public Vector3 g = Vector3.zero;
-    public Vector3 v0 = new Vector3(0,10,0);
-    public Vector3 currV = Vector3.zero;
-    public Vector2 boundary = Vector2.zero;
+    private Vector3 vel = Vector3.zero;
+    private Vector3 acc;
+    private Vector3 force;
+    
+    public float mass = 1f;
+    public float gravity = 9.8f;
+    public float boundary = 4.2f;
+    public Vector3 windForce = new Vector3 (0, 0.1f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y <= boundary.y * -1)
+        force = new Vector3(0, gravity * mass, 0);
+        if (Input.GetKeyDown(KeyCode.D)) force += windForce;
+        if (Input.GetKeyDown(KeyCode.A)) force -= windForce;
+
+        acc = force / mass;
+        Vector3 v0 = vel;
+        vel += acc;
+        Vector3 avg = (vel + v0) * 0.5f;
+        
+        transform.position += avg*Time.deltaTime;
+
+        if (transform.position.y < boundary * -1 && vel.y < 0)
         {
-            currV.y = 0;
-            transform.position = new Vector3(0,-4.2f,0);
+            vel.y = vel.y * -1;
         }
-        else
-        {
-            currV += g;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            AddforceUpside();
-        }
-        transform.position += currV * Time.deltaTime;
+
+        acc = Vector3.zero;
     }
-    void AddforceUpside()
-    {
-        currV += v0;
-    }
+
+
+
 }
